ac54a596f570caac8040e59f284b1b886e15ee74
Ted Leary <tedleary@Teds-MacBook-Air.local>
2018-11-22 10:00:10 -0800

    test

--- server/server.js
+++ server/server.js
@@ -8,16 +8,38 @@ var lions = [];
 var id = 0;
 
 var updateId = function(req, res, next){
-  id ++
-  Object.assign({id: id}, req.body)
+  if (!req.body.id) {
+    id ++
+    id = id + ''
+    req.body = Object.assign({id: id}, req.body)
+  }
   next();
 }
+
+app.use(morgan('dev'));
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.param('id', function(req, res, next, id) {
+  console.log(id);
+  var lion =_.find(lions, {id: id});
+  console.log(lion);
+if (lion) {
+  req.lion = lion
+  next();
+} else {
+  res.send();
+}
+
+});
 
 app.get('/lions', function(req, res) {
   res.json(lions);
 })
 
 app.get('/lions/:id', function(req, res) {
+  var lion = req.lion
   res.json(lion || {})
 });
 
@@ -26,6 +48,13 @@ app.post('/lions', updateId, function(req, res) {
 
   lions.push(lion);
   res.json(lion);
+
 })
+
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.status(500).send(err)
+  }
+});
 
 app.listen(4000);
